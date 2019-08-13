1cdc5b5bcf541a891e76391b41e8a2e9fd3b6641
Phil Ho <phil@philho.com>
2019-08-13 12:52:26 -0400

    Add registration page

--- application.py
+++ application.py
@@ -1,26 +1,71 @@
+"""Books - Project 1"""
 import os
+import sys
 
-from flask import Flask, session
-from flask_session import Session
+from flask import Flask, session, render_template, request
+from passlib.hash import sha256_crypt
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_session import Session
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+APP.config["SESSION_PERMANENT"] = False
+APP.config["SESSION_TYPE"] = "filesystem"
+Session(APP)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+ENGINE = create_engine(os.getenv("DATABASE_URL"))
+DB = scoped_session(sessionmaker(bind=ENGINE))
 
 
-@app.route("/")
+@APP.route("/")
 def index():
-    return "Project 1: TODO"
+    """Start page"""
+
+    return render_template("index.html")
+
+@APP.route("/register", methods=["GET", "POST"])
+def register():
+    """Registers a new user"""
+
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+
+        # Make sure all fields are filled out
+        if not username:
+            return "Username required"
+        if not password:
+            return "Password required"
+        if not confirm:
+            return "Must validate password"
+
+        # Make sure passwords match
+        if password != confirm:
+            return "Passwords do not match"
+
+        # Check if username already exists
+        rows = DB.execute("SELECT * FROM users WHERE upper(username) = upper(:username)", 
+                          {"username": username}).fetchall()
+        print(f"rows: {rows}", file=sys.stdout)
+
+        if rows:
+            return "Username already exists"
+
+        # Hash password and store
+        hashed_password = sha256_crypt.encrypt(password)
+
+        DB.execute("INSERT INTO users (username, password) values (:username, :password)",
+                   {"username": username, "password": hashed_password})
+        DB.commit()
+
+        return "Successfully registered user"
