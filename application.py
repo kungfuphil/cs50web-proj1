537817c5ab0978dbbc6f638822700b932b613224
Phil Ho <phil@philho.com>
2019-08-24 14:23:01 -0400

    Add book detail page

--- application.py
+++ application.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request, redirect, url_for, abort
 from passlib.hash import sha256_crypt
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -30,6 +30,26 @@ def index():
     """Start page"""
 
     return render_template("index.html")
+
+@APP.route("/book/<isbn>")
+@login_required
+def book(isbn):
+    """Book Page: When users click on a book from the results of the search page, they should be taken to a book page, with details about the book: its title, author, publication year, ISBN number, and any reviews that users have left for the book on your website."""
+
+    book = DB.execute("""SELECT  b.title, a.name, b.year, b.isbn
+                FROM books b
+                INNER JOIN authors a
+                ON b.author_id = a.author_id
+                WHERE b.isbn = :isbn""",
+                {"isbn": isbn}).fetchone()
+
+    # TODO: Have this return a nicer 404 page
+    # Check to see if it's a valid isbn, if not return 404
+    if book is None:
+        abort(404)
+        
+    # Otherwise, display title, author, year, isbn (for now)
+    return render_template("book.html", book=book)
 
 @APP.route("/login", methods=["GET", "POST"])
 def login():
@@ -115,9 +135,27 @@ def register():
 @login_required
 def search():
     """Search books by title, author, and/or isbn"""
-    title = request.args.get("title")
-    author = request.args.get("author")
-    isbn = request.args.get("isbn")
+
+    is_querying = False
+
+    # For whatever reason, ternary operators don't work correctly here.
+    if request.args.get("title"):
+        is_querying = True
+        title = f"%{request.args.get('title').lower()}%"
+    else:
+        title = "%"
+
+    if request.args.get('author'):
+        is_querying = True
+        author = f"%{request.args.get('author').lower()}%"
+    else:
+        author = "%"
+
+    if request.args.get('isbn'):
+        is_querying = True
+        isbn = f"%{request.args.get('isbn').lower()}%"
+    else:
+        isbn = "%"
 
     query = """SELECT b.title, a.name, b.year, b.isbn
                 FROM books b
@@ -127,10 +165,35 @@ def search():
                 AND LOWER(a.name) LIKE :author
                 AND LOWER(b.isbn) LIKE :isbn"""
 
-    title = f"%{title.lower()}%" if not None else ""
-    author = f"%{author.lower()}%" if not None else ""
-    isbn = f"%{isbn.lower()}%" if not None else  ""
-
-    books = DB.execute(query, {"title": title, "author": author, "isbn": isbn}).fetchall()
+    if is_querying:
+        books = DB.execute(query, {"title": title, "author": author, "isbn": isbn}).fetchall()
+    else:
+        books = []
 
     return render_template("search.html", books=books)
+
+
+
+
+"""Review Submission: On the book page, users should be able to submit a review: consisting of a rating on a scale of 1 to 5, as well as a text component to the review where the user can write their opinion about a book. Users should not be able to submit multiple reviews for the same book.
+
+Goodreads Review Data: On your book page, you should also display (if available) the average rating and number of ratings the work has received from Goodreads.
+
+API Access: If users make a GET request to your website’s /api/<isbn> route, where <isbn> is an ISBN number, your website should return a JSON response containing the book’s title, author, publication date, ISBN number, review count, and average score. The resulting JSON should follow the format:
+{
+    "title": "Memory",
+    "author": "Doug Lloyd",
+    "year": 2015,
+    "isbn": "1632168146",
+    "review_count": 28,
+    "average_score": 5.0
+}
+If the requested ISBN number isn’t in your database, your website should return a 404 error.
+
+You should be using raw SQL commands (as via SQLAlchemy’s execute method) in order to make database queries. You should not use the SQLAlchemy ORM (if familiar with it) for this project.
+
+In README.md, include a short writeup describing your project, what’s contained in each file, and (optionally) any other additional information the staff should know about your project.
+
+If you’ve added any Python packages that need to be installed in order to run your web application, be sure to add them to requirements.txt!
+
+Beyond these requirements, the design, look, and feel of the website are up to you! You’re also welcome to add additional features to your website, so long as you meet the requirements laid out in the above specification!"""
